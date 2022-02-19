62ed0c3675babf37f942ca9876b4d910930e3d8e
tlannigan <tristanlannigan@gmail.com>
2022-02-19 16:07:16 -0700

    Set scheduled gas price status updates

--- events/ready.js
+++ events/ready.js
@@ -4,18 +4,23 @@ const axios = require('axios').default
 require("dotenv").config()
 const token = process.env.ETH_TOKEN
 
+// Set status to ETH gas price
+async function setGasStatus(client) {
+	axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${token}`)
+		.then(res => {
+			client.user.setActivity(`Gas Price: ${res.data.result.ProposeGasPrice} gwei`, { type: 'WATCHING' })
+		})
+		.catch(error => {
+			console.error(error)
+		})
+}
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`${client.user.tag} has ascended.`)
-		// Set status to ETH gas price
-		axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${token}`)
-            .then(res => {
-                client.user.setActivity(`Gas Price: ${res.data.result.ProposeGasPrice} gwei`, { type: 'WATCHING'})
-            })
-            .catch(error => {
-                console.error(error)
-            })
+		setGasStatus(client)
+		setInterval(() => setGasStatus(client), 10000)
 	},
 }
