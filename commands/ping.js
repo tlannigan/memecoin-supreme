62d21391a2ce5c10ad6c07298c00d1d7fdf07844
tlannigan <tristanlannigan@gmail.com>
2022-02-19 13:18:24 -0700

    Reformatted event handling

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Alive and well :)'),
+        .setDescription('Check bot status.'),
     async execute(interaction) {
         await interaction.reply('Alive and well :)');
     },
--- index.js
+++ index.js
@@ -8,9 +8,21 @@ const token = process.env.BOT_TOKEN
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+// Register event handlers
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`)
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args))
+    } else {
+        client.on(event.name, (...args) => event.execute(...args))
+    }
+}
 
+// Register commands
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     // Set a new item in the collection
@@ -19,25 +31,25 @@ for (const file of commandFiles) {
 }
 
 // On ready
-client.once('ready', () => {
-    console.log('Memecoin Supreme has ascended.')
-})
+// client.once('ready', () => {
+//     console.log('Memecoin Supreme has ascended.')
+// })
 
 // Handle bot commands
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return
+// client.on('interactionCreate', async interaction => {
+//     if (!interaction.isCommand()) return
 
-    const command = client.commands.get(interaction.commandName);
+//     const command = client.commands.get(interaction.commandName);
 
-    if (!command) return
+//     if (!command) return
 
-    try {
-        await command.execute(interaction)
-    } catch (error) {
-        console.error(error)
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-})
+//     try {
+//         await command.execute(interaction)
+//     } catch (error) {
+//         console.error(error)
+//         await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+//     }
+// })
 
 // Authenticate with Discord
 client.login(token)
