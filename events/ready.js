99c0c75bbd4f3459a5da83e368d6c677328de5bd
tlannigan <tristanlannigan@gmail.com>
2022-02-19 15:00:53 -0700

    Set bot status to current gas price onReady

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,21 @@
-// Called when application is ready to be used
+const axios = require('axios').default
+
+// Load .env variables
+require("dotenv").config()
+const token = process.env.ETH_TOKEN
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`${client.user.tag} has ascended.`)
+		// Set status to ETH gas price
+		axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${token}`)
+            .then(res => {
+                client.user.setActivity(`Gas Price: ${res.data.result.ProposeGasPrice} gwei`, { type: 'WATCHING'})
+            })
+            .catch(error => {
+                console.error(error)
+            })
 	},
 }
