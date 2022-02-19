212f1a786ffa8d93c89ae85fc88fb7eb2cc824cd
tlannigan <tristanlannigan@gmail.com>
2022-02-19 12:45:18 -0700

    Reformat commands to command handling in index.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Alive and well :)'),
     async execute(interaction) {
         await interaction.reply('Alive and well :)');
     },
--- index.js
+++ index.js
@@ -26,11 +26,16 @@ client.once('ready', () => {
 // Handle bot commands
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
-    
-    const { commandName } = interaction;
 
-    if (commandName === 'ping') {
-        await interaction.reply('Alive and well :)');
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+        console.error(error)
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 })
 
