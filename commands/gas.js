b17e6e18b83db715290ac37d68f96600108a2895
tlannigan <tristanlannigan@gmail.com>
2022-02-19 14:02:50 -0700

    Add /price and /gas commands using Etherscan API

--- commands/gas.js
+++ commands/gas.js
@@ -1,10 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const axios = require('axios').default
+
+// Load .env variables
+require("dotenv").config()
+const token = process.env.ETH_TOKEN
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('gas')
         .setDescription('Returns estimated gas prices'),
     async execute(interaction) {
-        await interaction.reply('Wow only 3 gwei!')
+        axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${token}`)
+            .then(res => {
+                interaction.reply(`Gas Price: ${res.data.result.ProposeGasPrice} gwei`)
+            })
+            .catch(error => {
+                console.error(error)
+            })
     },
 }
--- commands/price.js
+++ commands/price.js
@@ -1,10 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const axios = require('axios').default
+
+// Load .env variables
+require("dotenv").config()
+const token = process.env.ETH_TOKEN
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('price')
         .setDescription('Returns the price of ETH'),
     async execute(interaction) {
-        await interaction.reply('Damn $6000 USD! To the moon!')
+        axios.get(`https://api.etherscan.io/api?module=stats&action=ethprice&apikey=${token}`)
+            .then(res => {
+                interaction.reply(`ETH Price: $${res.data.result.ethusd}`)
+            })
+            .catch(error => {
+                console.error(error)
+            })
     },
 }
