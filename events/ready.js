1fc3667dd9f0b76f083f4a2a556cabf68c8fe952
tlannigan <tristanlannigan@gmail.com>
2022-02-23 21:49:04 -0700

    Add scheduled username change to current ETH price

--- events/ready.js
+++ events/ready.js
@@ -6,14 +6,26 @@ module.exports = {
 	async execute(client) {
 		console.log(`${client.user.tag} has ascended.`)
 
+		const ethPrice = () => etherscan.getPrice()
 		const gasPrice = () => etherscan.getGasPrice()
 
-		// Set activity status to current gas price
-		client.user.setActivity(`Gas Price: ${await gasPrice()} gwei`, { type: 'WATCHING' })
+		client.guilds.cache.each(async (snowflake, guildID) => {
+			const guild = client.guilds.cache.get(guildID)
+			const targetMember = guild.members.cache.get(client.user.id)
+			targetMember.setNickname(`ETH $${await ethPrice()} CAD`)
+		})
 
-		// Set activity status to current gas price every 60 seconds
+		// Set activity status to current gas price
+		client.user.setActivity(`Gas: ${await gasPrice()} gwei`, { type: 'WATCHING' })
+
+		// Set activity status to current gas price every 15 seconds
 		setInterval(async () => {
-			client.user.setActivity(`Gas Price: ${await gasPrice()} gwei`, { type: 'WATCHING' })
-		}, 60000)
+			client.guilds.cache.each(async (snowflake, guildID) => {
+				const guild = client.guilds.cache.get(guildID)
+				const targetMember = guild.members.cache.get(client.user.id)
+				targetMember.setNickname(`ETH $${await ethPrice()} CAD`)
+			})
+			client.user.setActivity(`Gas: ${await gasPrice()} gwei`, { type: 'WATCHING' })
+		}, 15000)
 	},
 }
