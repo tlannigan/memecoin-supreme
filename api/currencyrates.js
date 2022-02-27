e0aebaf7d691e044c03b729a1f2ab81d8ecbdd8a
tlannigan <tristanlannigan@gmail.com>
2022-02-27 11:39:58 -0700

    Add try/catch to API calls

--- api/currencyrates.js
+++ api/currencyrates.js
@@ -3,7 +3,12 @@ const { default: axios } = require('axios')
 module.exports = {
     // Return exchange rate from USD to CAD
     async getCADRate() {
-        const rateData = await axios.get('https://api.vatcomply.com/rates?base=USD')
-        return rateData.data.rates.CAD
+        try {
+            const rateData = await axios.get('https://api.vatcomply.com/rates?base=USD')
+            return rateData.data.rates.CAD
+        } catch (error) {
+            console.log(error)
+            return ''
+        }
     },
 }
--- api/etherscan.js
+++ api/etherscan.js
@@ -10,14 +10,27 @@ module.exports = {
             const ethPrice = ethData.data.result.ethusd
             const cadPrice = cadRate * ethPrice
             return cadPrice.toFixed(2)
-        } catch (error){
-            console.error(error.toJSON())
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data)
+                console.log(error.response.status)
+                console.log(error.response)
+            }
             return 'ETHERSCAN UNREACHABLE'
         }
     },
     // Return proposed ETH gas prices in gwei
     async getGasPrice() {
-        const ethData = await axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETH_TOKEN}`)
-        return ethData.data.result.ProposeGasPrice
+        try {
+            const ethData = await axios.get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.ETH_TOKEN}`)
+            return ethData.data.result.ProposeGasPrice
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data)
+                console.log(error.response.status)
+                console.log(error.response)
+            }
+            return 'ETHERSCAN UNREACHABLE'
+        }
     },
 }
